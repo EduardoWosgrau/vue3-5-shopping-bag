807a6808f8985c5d56888ebc17b6ec9048cdbe73
Eduardo Santos <eduardo.wosgrau@gmail.com>
2023-09-14 09:38:22 -0300

    axios get all products

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="nav">
     <router-link to="/">Início</router-link> -
-    <router-link to="/basket">Carrinho (0)</router-link> 
+    <router-link to="/basket">Carrinho (0)</router-link>
   </div>
   <router-view/>
 </template>
 
 <script>
+  import axios from 'axios';
 
   export default {
-
+    created() {
+      axios.get('https://fakestoreapi.com/products')
+        .then(response => {
+          console.log(response.data);
+      })
+    }
   }
-  
 </script>
-
 
 <style lang="scss">
 #app {
