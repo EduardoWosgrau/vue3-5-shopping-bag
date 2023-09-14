f02231fa4067b1109854a5bc97a386d8243138aa
Eduardo Santos <eduardo.wosgrau@gmail.com>
2023-09-14 14:40:01 -0300

    add product bag validation

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -7,9 +7,12 @@
         class="product"
       >
         <div class="product-image" :style="{backgroundImage: 'url(' + product.image + ')'}"></div>
-        <h4>{{ product.title }}</h4>
+        <h4>{{ product.title }}</h4>s
         <p class="price">US$ {{ product.price.toFixed(2) }}</p>
-        <button @click="addToBag(product)">Add to bag</button>
+        <button 
+          v-if="!isInBag(product)"
+          @click="addToBag(product)"
+        >Add to bag</button>
       </div>
     </div>
   </div>
@@ -21,12 +24,17 @@ export default {
   computed: {
     products(){
       return this.$store.state.products;
+    },
+    productsInBag(){
+      return this.$store.state.productsInBag;
     }
   },
   methods: {
     addToBag(product){
-      product.quantity = 1;
-      this.$store.dispatch('addToBag', product);
+      this.$store.dispatch('addToBag', {...product, quantity: 1});
+    },
+    isInBag(product){
+      return this.productsInBag.find(item => item.id === product.id)
     }
   }
 }
