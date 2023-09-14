108290a17ca006414dbe3ab01052567bfb292fb4
Eduardo Santos <eduardo.wosgrau@gmail.com>
2023-09-14 13:06:51 -0300

    load product | actions and mutations

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,32 @@
 import { createStore } from 'vuex'
+import axios from 'axios';
 
 export default createStore({
   state: {
-    
+    products: [],
+    productsInBag: [],
   },
   mutations: {
-    
+    loadProducts(state, products){
+      console.log(products)
+      state.products = products;
+    },
+    addToBag(state, product){
+      state.products.push(product);
+    },
   },
   actions: {
-
+    loadProducts({ commit }){
+      axios.get('https://fakestoreapi.com/products')
+      .then(response => {
+        commit('loadProducts', response.data);
+      })
+    },
+    addToBag({ commit, product }){
+      commit('addToBag', product)
+    }
   },
   modules: {
-    
+
   }
 })
